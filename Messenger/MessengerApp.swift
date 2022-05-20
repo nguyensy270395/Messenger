aa221abfcbed9b71ddda59d848af6fd3226fe73a
nguyensy270395 <99628647+nguyensy270395@users.noreply.github.com>
2022-05-20 12:00:30 +0700

    day2

--- Messenger/MessengerApp.swift
+++ Messenger/MessengerApp.swift
@@ -10,11 +10,30 @@ import SwiftUI
 @main
 struct MessengerApp: App {
     let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear {
+                    UIApplication.shared.hideKeyboardWhenTap()
+                }
         }
+    }
+}
+
+extension UIApplication {
+    func hideKeyboardWhenTap() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true // set to `false` if you don't want to detect tap during other gestures
     }
 }
--- Messenger/View/HomeView.swift
+++ Messenger/View/HomeView.swift
@@ -6,66 +6,125 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct HomeView: View {
     @State var search = ""
+    @State var index = 0
+    @State var showPlaceholder = true
+    
     var body: some View {
-        VStack(spacing: 0) {
-            VStack {
-                ZStack {
-                    HStack {
-                        Image("imageAccount")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 40, height: 40)
-                            .clipShape(Circle())
-                            .padding(.horizontal)
-                        Spacer(minLength: 0)
-                        
+        
+            VStack(spacing: 0) {
+                VStack {
+                    ZStack {
+                        HStack {
+                            Image("imageAccount")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                                .padding(.horizontal)
+                            Spacer(minLength: 0)
+                            
+                        }
+                        Text("Đoạn chat")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            
                     }
-                    Text("Đoạn chat")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        
+                    ZStack {
+                        if showPlaceholder {
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 22, height: 22)
+                                    .foregroundColor(.black.opacity(0.5))
+                                    
+                                Text("Tìm kiếm")
+                                    .font(.system(size: 21))
+                                    .fontWeight(.regular)
+                                    .foregroundColor(.black.opacity(0.5))
+                                Spacer()
+                                
+                            }
+                            .padding(.horizontal, 12)
+                        }
+                       
+                        TextField("", text: $search, onEditingChanged: { check in
+                            showPlaceholder = !check
+                        })
+                            .padding(12)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(.gray.opacity(0.11))
+                            .cornerRadius(13)
+     
+                    }
+                    .padding(.top, 5)
+                    .padding(.horizontal)
                 }
-                ZStack {
-                    HStack {
-                        Image(systemName: "magnifyingglass")
+                .padding(.bottom)
+               
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 16){
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                        UserChatView()
+                    }
+                    
+                }
+                HStack {
+                    Spacer(minLength: 0)
+                    VStack(spacing: 0) {
+                        Image(systemName: "message")
+                            .resizable()
+                            .frame(width: 27, height: 27)
+                            .foregroundColor(index == 0 ? Color("Color1") : .gray.opacity(0.9))
+                            .padding(.vertical, 6)
+                   
+                        Text("Đoạn chat")
+                            .font(.system(size: 14))
+                            .fontWeight(.regular)
+                            .foregroundColor(index == 0 ? Color("Color1") : .gray.opacity(0.9))
+                    }
+                    .onTapGesture {
+                        index = 0
+                    }
+                    Spacer(minLength: 0)
+                    VStack(spacing: 0){
+                        Image(systemName: "person.2")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 23, height: 23)
-                            .foregroundColor(.black.opacity(0.5))
-                            
-                        Text("Tìm kiếm")
-                            .font(.title2)
+                            .frame(width: 35, height: 35)
+                            .foregroundColor(index == 1 ? Color("Color1") : .gray.opacity(0.9))
+                            .padding(.top, 2)
+                        Text("Danh bạ")
+                            .font(.system(size: 14))
                             .fontWeight(.regular)
-                            .foregroundColor(.black.opacity(0.5))
-                        Spacer()
-                        
+                            .foregroundColor(index == 1 ? Color("Color1") : .gray.opacity(0.9))
+                            .padding(.top, 3)
                     }
-                    .padding(.horizontal, 12)
-                    TextField("", text: $search)
-                        .padding(12)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .background(.gray.opacity(0.11))
-                        .cornerRadius(13)
+                    .onTapGesture {
+                        index = 1
+                    }
+                    Spacer(minLength: 0)
                 }
-                .padding(.top, 5)
-                .padding(.horizontal)
             }
-            .padding(.bottom)
+            .ignoresSafeArea(.keyboard, edges: .bottom)
+            
            
-            ScrollView(.vertical, showsIndicators: false) {
-                UserChatView()
-            }
-            
-            
-            
-            
-            Spacer()
         }
+
         
-    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
@@ -80,19 +139,21 @@ struct UserChatView: View {
                 Image("user")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
-                    .frame(width: 80, height: 80)
+                    .frame(width: 75, height: 75)
                     .clipShape(Circle())
-                VStack(alignment: .leading){
-                    Text("Thanh Thanh")
-                        .font(.title2)
+                VStack(alignment: .leading, spacing: 0){
+                    Text("TPBank")
+                        .font(.system(size: 22))
                         .fontWeight(.medium)
                         .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                     Text("Message: Hello")
-                        .font(.caption)
+                        .font(.system(size: 14))
+                        .fontWeight(.regular)
+                        .foregroundColor(.black.opacity(0.7))
                 }
                 
             }
-            .padding()
+            .padding(.horizontal)
         }
     
 }
