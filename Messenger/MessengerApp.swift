1d37428b301e649ed6254643ef6e526fc0756a8a
nguyensy270395 <99628647+nguyensy270395@users.noreply.github.com>
2022-05-24 17:37:49 +0700

    Time

--- Messenger/MessengerApp.swift
+++ Messenger/MessengerApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+
 @main
 struct MessengerApp: App {
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate 
+    
     let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
